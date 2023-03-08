948873fa534a98f86e91eecd9907ebcdecc7b0d8
emmaroche <20088680@mail.wit.ie>
2023-03-08 16:35:00 +0000

    Add splash screen feature

---
+++ app/src/main/java/ie/setu/youtuberApp/activities/SplashScreenActivity.kt
@@ -0,0 +1,31 @@
+package ie.setu.youtuberApp.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import ie.setu.youtuberApp.R
+
+
+//Code resource to help create splash screen: https://www.geeksforgeeks.org/how-to-create-a-splash-screen-in-android-using-kotlin/
+@Suppress("DEPRECATION")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        // This is used to hide the status bar and make the splash screen full screen
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, YouTuberListActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
